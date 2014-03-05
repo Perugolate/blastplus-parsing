bc71fc1edcb10f9c29d0554c3a8872e1102a8996
Perugolate <Paul.johnston@fu-berlin.de>
2014-03-05 11:05:07 +0100

    converted usage to docstring

--- blastplus_taxa_barh.py
+++ blastplus_taxa_barh.py
@@ -1,13 +1,25 @@
 #! /usr/bin/python
-# takes 4 arguments: blast output in format 6; field no. for taxa descriptions; no. of taxa to plot; filename for plot
-# requires pandas and matplotlib
+"""
+Usage: blastplus_taxa_barh.py blast_output staxids_field no_taxa plot_filename
+
+blast_ouput: 	output from BLAST 2.2.28+ and later in outfmt 6 containing staxids 
+staxids_field:	field in blast output containing staxids (Genus species)
+no_taxa:	number of taxa to include in the horizontal bar plot
+plot_filename:	filename for plot .png
+
+example blast invocation which places staxids in field 12:
+blastx -query query.fasta -db refseq_protein -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send stitle staxids sscinames evalue" -out blast_out 	
+"""
 
 import sys
-from pandas import DataFrame
 import pandas
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+if len(sys.argv) < 4:
+   print __doc__
+   sys.exit(0)
 
 in_file = sys.argv[1]
 tax_field = int(sys.argv[2])
@@ -16,7 +28,7 @@ out_file = sys.argv[4]
 
 def blast_in(in_file):
   blast_data = pandas.read_table(in_file,header = None)
-  blast_data = DataFrame(blast_data)
+  blast_data = pandas.DataFrame(blast_data)
   return blast_data
 
 def genus(blast_data):
