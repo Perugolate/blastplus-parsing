4804018057687a2af99df7d55917d884cb3a1295
Perugolate <Paul.johnston@fu-berlin.de>
2014-03-06 13:51:09 +0100

    modified docstring

--- blastplus_taxa_barh.py
+++ blastplus_taxa_barh.py
@@ -13,7 +13,9 @@ Arguments:
 		filename for plot .png
 
 example blast invocation which places staxids in field 12:
-blastx -query query.fasta -db refseq_protein -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send stitle staxids sscinames evalue" -out blast_out 	
+blastx -query query.fasta -db refseq_protein -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send stitle staxids sscinames evalue" -out blast_out
+
+Paul Johnston (Paul.Johnston@fu-berlin.de)
 """
 
 import sys
