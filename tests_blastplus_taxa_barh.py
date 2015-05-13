55b22483cc5d7ac7cd8eb6630ea41ddf8216afd3
Perugolate <Paul.johnston@fu-berlin.de>
2015-05-14 00:15:31 +0200

    added tests

---
+++ tests_blastplus_taxa_barh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pandas
+import unittest
+from blastplus_taxa_barh import *
+
+__author__ = "Paul Johnston"
+__contact__ = "paul.johnston[at]fu-berlin.de"
+
+class test_blast_parse(unittest.TestCase):
+    
+    def test_blast_in(self):
+        known_frame = pandas.io.pickle.read_pickle("tests/test_frame.pkl")
+        test_frame = blast_in("tests/outfmt6.all.uniq")
+        try:
+            pandas.util.testing.assert_frame_equal(known_frame, test_frame, check_names=False)
+            return True
+        except (AssertionError, ValueError, TypeError):
+            return False
+
+    def test_genus(self):
+        known_series = pandas.io.pickle.read_pickle("tests/test_series.pkl")
+        known_frame = pandas.io.pickle.read_pickle("tests/test_frame.pkl")
+        test_series = genus(known_frame, int(12), int(15))
+        try:
+            pandas.util.testing.assert_series_equal(known_series, test_series)
+            return True
+        except (AssertionError, ValueError, TypeError):
+            return False
+
+if __name__ == '__main__':
+    unittest.main()
