9b837a6af580ed0d206098c86ee1526ac8f703c8
perugolate <paul.johnston@fu-berlin.de>
2015-05-14 00:22:03 +0200

    added tests, test data

--- tests_blastplus_taxa_barh.py
+++ tests_blastplus_taxa_barh.py
@@ -10,8 +10,8 @@ __contact__ = "paul.johnston[at]fu-berlin.de"
 class test_blast_parse(unittest.TestCase):
     
     def test_blast_in(self):
-        known_frame = pandas.io.pickle.read_pickle("tests/test_frame.pkl")
-        test_frame = blast_in("tests/outfmt6.all.uniq")
+        known_frame = pandas.io.pickle.read_pickle("test_data/test_frame.pkl")
+        test_frame = blast_in("test_data/outfmt6.all.uniq")
         try:
             pandas.util.testing.assert_frame_equal(known_frame, test_frame, check_names=False)
             return True
@@ -19,8 +19,8 @@ class test_blast_parse(unittest.TestCase):
             return False
 
     def test_genus(self):
-        known_series = pandas.io.pickle.read_pickle("tests/test_series.pkl")
-        known_frame = pandas.io.pickle.read_pickle("tests/test_frame.pkl")
+        known_series = pandas.io.pickle.read_pickle("test_data/test_series.pkl")
+        known_frame = pandas.io.pickle.read_pickle("test_data/test_frame.pkl")
         test_series = genus(known_frame, int(12), int(15))
         try:
             pandas.util.testing.assert_series_equal(known_series, test_series)
